3f564ab99000cf6377029c4e0b51723d4da368b4
Marc Enschede <marc@marcenschede.nl>
2019-09-30 17:30:42 +0200

    Initial Commit

---
+++ ios13exp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios13exp2
+//
+//  Created by Marc Enschede on 30/09/2019.
+//  Copyright © 2019 Marc Enschede. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios13exp2Tests/ios13exp2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios13exp2Tests.swift
+//  ios13exp2Tests
+//
+//  Created by Marc Enschede on 30/09/2019.
+//  Copyright © 2019 Marc Enschede. All rights reserved.
+//
+
+import XCTest
+@testable import ios13exp2
+
+class ios13exp2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
